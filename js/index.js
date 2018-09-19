3dc04aa91aae0a7d96ab962c7de5867ef6370251
kankanol1 <1317894008@qq.com>
2018-09-19 16:41:08 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+
+
+
+var data = [
+    {
+        url: "bodyone",
+        height: 1900
+    },
+    {
+        url: "two",
+        height: 950
+    },
+    {
+        url: "store",
+        height: 595
+    },
+    {
+        url: "works",
+        height: 590
+    },
+    {
+        url: "event",
+        height: 590
+    },
+    {
+        url: "evaluate",
+        height: 590
+    }
+];
+var oBox = document.getElementById('box');
+var oBtn = document.querySelectorAll('.wrap .nav li');
+
+var flag = oBtn[0];
+for(let i=0;i<oBtn.length;i++){
+    oBtn[i].addEventListener('touchstart',function(){
+        flag.className = '';
+        var str = '<iframe class="two" src="'+ data[i].url +'.html" frameborder="0" width="100%" height="'+ data[i].height +'rem"></iframe>';
+        flag = this;
+        oBox.innerHTML = str;
+        flag.className = 'active';
+    },false);
+}
